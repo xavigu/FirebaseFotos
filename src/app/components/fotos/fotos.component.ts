5803118d3d1a8e54da1a2708a2a85169c7984cc2
xavigu <xavigu91@gmail.com>
2019-10-10 18:02:42 +0200

    Show all images of the firebase in Fotos page

--- src/app/components/fotos/fotos.component.ts
+++ src/app/components/fotos/fotos.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
+export interface Item { 
+  name: string; 
+  url: string;
+}
 @Component({
   selector: 'app-fotos',
   templateUrl: './fotos.component.html',
@@ -7,8 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FotosComponent implements OnInit {
 
-  constructor() { }
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
 
+  constructor(private readonly afs: AngularFirestore) {
+    this.itemsCollection = afs.collection<Item>('img'); //donde tenemos la colección de imagenes
+    // .valueChanges() is simple. It just returns the 
+    // JSON data without metadata. If you need the 
+    // doc.id() in the value you must persist it your self
+    // or use .snapshotChanges() instead. See the addItem()
+    // method below for how to persist the id with
+    // valueChanges()
+    this.items = this.itemsCollection.valueChanges();
+  }
   ngOnInit() {
   }
 
